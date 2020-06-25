416e4b3e85133737f34c50b0314691a657454b04
GuilhermeDiasSilva <59282687+GuilhermeDiasSilva@users.noreply.github.com>
2020-06-25 10:42:53 -0300

    Update and rename JavaScript/aula12ex/ex014/script.js to Javascript/aula12ex/ex014/script.js

--- Javascript/aula12ex/ex014/script.js
+++ Javascript/aula12ex/ex014/script.js
@@ -2,9 +2,9 @@ function carregar(){
     var msg  = window.document.getElementById('msg')
     var img  = window.document.getElementById('imagem')
     var data = new Date()
-    //var hora = data.getHours()
+    var hora = data.getHours()
     var minutos = data.getMinutes()
-    var hora = 21
+    
     msg.innerHTML = `Agora são ${hora}:${minutos} horas. `
     if (hora >= 0 && hora < 12){
         //BOM DIA
